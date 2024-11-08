7cf5714e25178d4a03f2ba6445c56ddaa8b8b7ef
Miguel Aldarias <aldarias.88@gmail.com>
2024-11-08 19:48:22 +0100

    ip publica

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 import aiomysql
 
 DATABASE_CONFIG = {
-    'host': '192.168.0.16',   
+    'host': '13.51.160.6',   
     'port': 3306,
     'user': 'gym_root',      
     'password': 'Teletrabajo88',
