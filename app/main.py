12593be667065322f777b6fa30286711b7516ebd
Miguel Aldarias <aldarias.88@gmail.com>
2024-11-15 09:09:42 +0100

    cors

--- app/main.py
+++ app/main.py
@@ -22,7 +22,6 @@ origins = [
     "http://localhost:3000",  # Frontend en desarrollo
     "http://127.0.0.1:3000",
     "http://13.51.160.6:8000", # IP pública de tu instancia
-    "http://frontend-examen-final.s3-website.eu-north-1.amazonaws.com/registros/clientes"
 ]
 
 app.add_middleware(
